993e4b9b559ce801f6d5560708af8206b7a23fc4
sherstyuchenko <104486840+sherstyuchenko@users.noreply.github.com>
2022-09-22 12:21:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 'use strict';
 
-/*
-
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '💃🏼 Correct number!';
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-
-*/
-
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 10;
 let highscore = 0;
